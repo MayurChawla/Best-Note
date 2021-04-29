1d500ee199e18b0f2a18e0104d65e799347ff7d1
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-29 10:05:35 +0530

    editor

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -8,10 +8,30 @@ import styles from './styles';
 class EditorComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            text: '',
+            title: '',
+            id: ''
+        };
     }
     render() {
-        return(<div>Hello from the Editor</div>);
+        const {classes} = this.props;
+        return(
+            <div className={classes.editorContainer}>
+                <ReactQuill 
+                    value={this.state.text}
+                    onChange={this.updateBody}></ReactQuill>
+            </div>
+        );
     }
+    updateBody = async(val)=>{
+        await this.setState({text:val});
+        this.update();
+    };
+    update = debounce(()=>{
+        console.log("updating database");
+        //Comback Later
+    }, 1500);
 }
 
 export default withStyles(styles)(EditorComponent);
