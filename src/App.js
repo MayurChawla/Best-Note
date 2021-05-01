9ba69bf1e65875852340be43316f18c1486fc475
MayurChawla <mr.mayurchawla@gmail.com>
2021-05-01 12:02:28 +0530

    build

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
   }
   deleteNote = async (note) =>{
     const noteIndex = this.state.notes.indexOf(note);
-    await this.setState({notes: this.state.notes.filter(_note=> _note !== _note)})
+    await this.setState({notes: this.state.notes.filter(_note=> _note !== note)})
     if(this.state.selectedNoteIndex === noteIndex) {
       this.setState({selectedNoteIndex: null, selectedNote: null});
     }
